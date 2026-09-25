d34181a77b19e82aed3e0719a53eeda47f0f1ae8
deliiivvv <deliviaanggraini11@gmail.com>
2026-09-25 15:46:25 +0700

    Perbaiki fungsi RSVP

--- script.js
+++ script.js
@@ -82,3 +82,36 @@ musicButton.addEventListener("click", function () {
     }
 
 });
+const rsvpForm = document.querySelector(".rsvp-form");
+
+rsvpForm.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const nama = rsvpForm.querySelector("input").value;
+    const kehadiran = rsvpForm.querySelectorAll("select")[0].value;
+    const jumlah = rsvpForm.querySelectorAll("select")[1].value;
+
+    fetch("https://script.google.com/macros/s/AKfycbz5qH0qvwL5SzSg4iX-atFx5IIXn_s5QrDsg1WLJ57MeA8x8A0bTgvwWW_VpOj8Gf8q4w/exec", {
+        method: "POST",
+
+        body: JSON.stringify({
+            nama: nama,
+            kehadiran: kehadiran,
+            jumlah: jumlah
+        })
+    })
+    .then(function () {
+
+        alert("Terima kasih! Konfirmasi kehadiran berhasil dikirim.");
+
+        rsvpForm.reset();
+
+    })
+    .catch(function () {
+
+        alert("Maaf, terjadi kesalahan. Silakan coba lagi.");
+
+    });
+
+});
